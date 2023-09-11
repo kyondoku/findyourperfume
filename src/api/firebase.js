f26a1295592577f6d5b988a49ff7906878c2bda0
kyondoku <kyondoku@gmail.com>
2023-09-11 16:23:12 +0900

    admin 권한여부에 따라 버튼 표시 / 버튼 컴포넌트로 분리

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import { child, get, getDatabase, ref } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,6 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const db = getDatabase(app);
 
 export function login() {
   return (
@@ -31,7 +33,25 @@ export function logout() {
 }
 
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  // checkIsAdmin이 비동기함수이므로
+  onAuthStateChanged(auth, async (user) => {
+    // 1. 로그인한 경우
+    const updatedUser = user ? await checkIsAdmin(user) : null;
+    callback(updatedUser);
+  });
+}
+
+// 네트워크 통신을 필요로 하니까 async
+async function checkIsAdmin(user) {
+  // 2. 어드민 권한여부 체크
+  // 3. {...user, isAdmin: true/false}
+
+  return get(ref(db, "admins")).then((snapshot) => {
+    if (snapshot.exists()) {
+      const admins = snapshot.val();
+      const isAdmin = admins.includes(user.uid);
+      return { ...user, isAdmin };
+    }
+    return user;
   });
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { TbPerfume } from "react-icons/tb";
 import { BsFillPencilFill } from "react-icons/bs";
 import { login, logout, onUserStateChange } from "../api/firebase";
 import User from "./User";
+import Button from "./ui/Button";
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -12,7 +13,11 @@ export default function Navbar() {
     onUserStateChange(
       //(user) => {setUser(user);}
       //전달받는 인자와 호출에 전달하는 인자가 같으므로
-      setUser
+      //setUser
+      (user) => {
+        console.log(user);
+        setUser(user);
+      }
     );
   }, []);
 
@@ -24,13 +29,15 @@ export default function Navbar() {
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className="text-2xl">
-          <BsFillPencilFill />
-        </Link>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
       </nav>
     </header>
   );
